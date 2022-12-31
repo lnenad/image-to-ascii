44688e744985324e38c792b1ae4115b65265dab8
Grant Lanham <contact@grantlanham.com>
2022-12-31 14:26:40 -0500

    Improve direct executable support and updates
    The following updates were performed:
    1. Upgrade all packages
    2. Provide an in-text help menu
    3. Move the HTML file to raw text in the program
    4. Cleaned up some warnings on not handling Results
    5. Improved output filename logic to always include .txt

--- README.md
+++ README.md
@@ -9,25 +9,44 @@ I'm learning Rust and thought this would be an easy enough project to do and lea
 
 ## Installation
 
-If you have cargo dependencies should be installed automatically by running
+If you have cargo dependencies should be installed automatically by building
+
 ```bash
-$ cargo run
+$ cargo build --release
 ```
+
+From there, navigate to the `target/release` and execute the binary
+
+```bash
+$ imagetoascii.exe
+```
+
+If no arguments are provided, the help menu in the next section is printed
+
 ## Usage
 
 The 'ita' cmd utility accepts the following arguments. 
 
 ```
--i|-image      [image path]   Input image
--o|-output     [file name]    Output file name
--r|-resolution [integer]      Resolution (reverse linear scale, it's the divider of the original image resolution, the bigger the number the smaller the ascii output)
--p|-preview                   Flag that is used to generate an HTML preview (the name of the file will be the same as the input image file with an html extension)
+-i|-image      [image path]   The input image file name
+-o|-output     [file name]    Output file name, if none is provided then will print to the stdout. Note: if the output file does not end in '.txt', that will be appended to it.
+-r|-resolution [integer]      Resolution: Perfomed with reverse linear scale, it's the divider of the original image resolution, the bigger the number the smaller the ascii output
+-p|-preview                   Flag that is used to generate an HTML preview and will open in your OS's default application (the name of the file will be the same as the input image file with an html extension)
 ```
 
 ## Example
+The program can be run directly from cargo
 ```bash
 $ cargo run -- -i image.png -o image.txt -r 5 -p  
 ```
+
+Or the program can be run by building and executing the image
+```bash
+$ cargo build --release
+$ cd ./target/relase
+$ imagetoascii -i image.png -o image.txt -r 5 -p  
+```
+
 The given example analyzes the image.png file and outputs the ascii art to image.txt. It will resize the original image to a fifth of it's original size and it will generate an HTML preview.
 
 ## Tests
@@ -36,7 +55,8 @@ I'd like to write basic tests soon just for practice
 
 ## Contributors
 
-Nenad Lukic - [lnenad](http://github.com/lnenad)
+Nenad Lukic - [lnenad](https://github.com/lnenad)
+Grant Lanham - [glanham-jr](https://github.com/glanham-jr)
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+extern crate image;
 extern crate imagetoascii;
 extern crate time;
-extern crate image;
 
 use imagetoascii::cmd::Parser;
 use std::error::Error;
@@ -8,42 +8,105 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-use image::GenericImage;
 use image::imageops::FilterType;
+use image::GenericImageView;
 
 fn main() {
     let (arguments, flags) = Parser::new().parse();
-    let image_path = arguments.get("image").or(arguments.get("i")).expect("No image argument provided");
 
-    println!("{:?}{:?}", arguments, flags);
+    if arguments.len() == 0 {
+        print_help();
+        return;
+    }
 
-    let output = arguments.get("output").or(arguments.get("o")).expect("No output argument provided");
+    let image_path = arguments.get("image").or(arguments.get("i"));
+
+    if image_path.is_none() {
+        println!("No image was provided, use '-i' or '-input' to specify");
+        print_help();
+        return;
+    }
+
+    let output_path = arguments.get("output").or(arguments.get("o"));
 
     let resolution = match arguments.get("resolution").or(arguments.get("r")) {
         Some(val) => val.parse::<u32>().unwrap(),
-        None => 5
+        None => 5,
     };
 
-    let start = time::precise_time_ns();
-    let contents = convert_to_ascii(image_path, resolution, output);
-    let end = (time::precise_time_ns() - start) / 1000000;
+    let preview = flags.contains(&"preview".to_owned()) || flags.contains(&"p".to_owned());
 
-    if flags.contains(&"preview".to_owned()) || flags.contains(&"p".to_owned()) {
-        generate_preview(&output.replace(".txt", ".html"), &contents);
-    };
-
-    println!("Completed in: {}ms", end);
+    run(image_path.unwrap(), resolution, output_path, preview);
 }
 
-fn convert_to_ascii(image_url: &str, resolution: u32, output: &str) -> String {
-    let img = image::open(&Path::new(image_url)).unwrap();
+fn run(image_path: &str, resolution: u32, output_path: Option<&String>, preview: bool) {
+    // let image_path_txt =
+    let start = time::precise_time_ns();
+    let art = convert_to_ascii(&image_path, resolution, output_path.is_some());
+    let end = (time::precise_time_ns() - start) / 1000000;
+
+    match output_path {
+        Some(op) => {
+            let file_name = if op.ends_with(".txt") {
+                op.to_owned()
+            } else {
+                format!("{}{}", op, ".txt".to_owned())
+            };
+            match write_to_file(&file_name, &art) {
+                Ok(_) => (),
+                _ => (),
+            };
+
+            if preview {
+                let preview_file_name = &file_name.replace(".txt", ".html");
+                match generate_preview(preview_file_name, &art) {
+                    Ok(_) => {
+                        println!("Output HTML File: {}", preview_file_name);
+                        match open::that(preview_file_name) {
+                            Ok(_) => (),
+                            Err(err) => eprint!("Error occured while opening file: {}", err),
+                        };
+                    }
+                    Err(err) => eprintln!("Error ocurred while generating file: {}", err),
+                };
+            };
+            println!("Completed in: {}ms", end);
+        }
+        None => println!("{}", art),
+    }
+}
+
+fn print_help() {
+    println!(concat!(
+        "-i|-image      [image path]   The input image file name\n",
+        "-o|-output     [file name]    Output file name, if none is provided then will print to the stdout. Note: if the output file does not end in '.txt', that will be appended to it.\n",
+        "-r|-resolution [integer]      Resolution: Perfomed with reverse linear scale, it's the divider of the original image resolution, the bigger the number the smaller the ascii output\n",
+        "-p|-preview                   Flag that is used to generate an HTML preview and will open in your OS's default application (the name of the file will be the same as the input image file with an html extension)"
+    ));
+}
+
+fn convert_to_ascii(image_path: &str, resolution: u32, print_info: bool) -> String {
+    if print_info {
+        println!("Input File: {}", image_path);
+    }
+    let img = image::open(&Path::new(image_path)).unwrap();
     let character_set: [&str; 11] = ["@", "#", "0", "O", "L", ";", ":", ".", ",", "'", " "];
     let mut art = String::new();
     let mut last_y = 0;
 
-    let small_img = img.resize(img.width() / resolution, img.height() / resolution, FilterType::Nearest);
+    let small_img = img.resize(
+        img.width() / resolution,
+        img.height() / resolution,
+        FilterType::Nearest,
+    );
 
-    println!("Original size: {:?}   Reduced: {:?}", img.dimensions(), small_img.dimensions());
+    if print_info {
+        println!(
+            "Original size: {:?}   Reduced: {:?}",
+            img.dimensions(),
+            small_img.dimensions()
+        );
+    }
 
     for pixel in small_img.pixels() {
         if last_y != pixel.1 {
@@ -51,38 +114,36 @@ fn convert_to_ascii(image_url: &str, resolution: u32, output: &str) -> String {
             last_y = pixel.1;
         }
 
-        let pixel_data = pixel.2.data;
-        let brightness:f64 = ((pixel_data[0] as u64 + pixel_data[1] as u64 + pixel_data[2] as u64) / 3) as f64;
-        let character_position = ((brightness/255.0) * (character_set.len()  - 1) as f64 ).round() as usize;
+        let pixel_data = pixel.2;
+        let brightness: f64 =
+            ((pixel_data[0] as u64 + pixel_data[1] as u64 + pixel_data[2] as u64) / 3) as f64;
+        let character_position =
+            ((brightness / 255.0) * (character_set.len() - 1) as f64).round() as usize;
         art.push_str(character_set[character_position])
     }
-
-    match write_to_file(output,&art) {
-        Ok(_) => (),
-        _ => ()
-    };
 
     art
 }
 
-fn write_to_file(file_name: &str, contents: &String) -> Result<String, Box<Error>> {
+fn write_to_file(file_name: &String, contents: &String) -> Result<String, Box<dyn Error>> {
     let mut file = File::create(file_name)?;
-    if true == false {
-        return Err(Box::from("Is not a directory!"));
-    }
-
     file.write_all(contents.as_bytes())?;
 
     Ok("Done".into())
 }
 
-fn generate_preview(file_name: &str, contents: &String) -> Result<String, Box<Error>> {
-    let mut src = File::open("preview.html")?;
-    let mut data = String::new();
-    src.read_to_string(&mut data)?;
-    drop(src);  // Close the file early
-
-    let new_data = data.replace("{templateContents}", &*contents);
+fn generate_preview(file_name: &String, contents: &String) -> Result<String, Box<dyn Error>> {
+    let html = concat!(
+        "<!DOCTYPE html>\n",
+        "<!-- saved from url=(0053)https://www.google.rs/_/chrome/newtab?espv=2&ie=UTF-8 -->\n",
+        "<html lang=\"en-RS\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n",
+        "</head>\n",
+        "<body style=\"font-size: 2.5pt;\">\n",
+	    "   <pre style=\"color: black; letter-spacing: 1px;font-weight: bold;\">{templateContents}</pre>\n",
+        "</body>\n",
+        "</html>",
+    );
+    let new_data = html.replace("{templateContents}", &*contents);
 
     let mut dst = File::create(&file_name)?;
     dst.write(new_data.as_bytes())?;
